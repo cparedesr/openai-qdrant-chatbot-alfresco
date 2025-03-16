3309997ab398c4804298e509a7450212f346b340
cparedesr <carlosp_rouyet@outlook.com>
2025-03-16 03:26:14 +0100

    Update PythonApiService.java

--- alfresco-vector-embeddings-listener/src/main/java/com/alfresco/vector/embeddings/listener/service/PythonApiService.java
+++ alfresco-vector-embeddings-listener/src/main/java/com/alfresco/vector/embeddings/listener/service/PythonApiService.java
@@ -3,7 +3,10 @@ package com.alfresco.vector.embeddings.listener.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -52,6 +55,29 @@ public class PythonApiService {
             LOGGER.info("Respuesta de Python (delete): {}", response.getBody());
         } catch (Exception e) {
             LOGGER.error("Error al llamar a la API de Python (delete) en {}: {}", url, e.getMessage());
+        }
+    }
+
+    public void updateVectorEmbeddings(String nodeId, String text) {
+        String url = String.format("http://%s:%d/index", pythonApiHost, pythonApiPort);
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("nodeid", nodeId);
+        requestBody.put("text", text);
+
+        try {
+            // Construimos la request como un HttpEntity para la llamada PUT
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+            ResponseEntity<String> response =
+                    restTemplate.exchange(url, HttpMethod.PUT, requestEntity, String.class);
+
+            LOGGER.info("Respuesta de Python (update): {}", response.getBody());
+        } catch (Exception e) {
+            LOGGER.error("Error al llamar a la API de Python (update) en {}: {}", url, e.getMessage());
         }
     }
 }
