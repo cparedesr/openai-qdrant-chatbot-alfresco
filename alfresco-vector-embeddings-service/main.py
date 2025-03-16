e0401a37daae7ffc2cc24cbba8ea9a31c944e403
cparedesr <carlosp_rouyet@outlook.com>
2025-03-16 02:46:54 +0100

    changes aplication alfresco-vector-embbedings-service

--- alfresco-vector-embeddings-service/main.py
+++ alfresco-vector-embeddings-service/main.py
@@ -11,4 +11,4 @@ app = gr.mount_gradio_app(app, fresquito_app, path="/chatbot")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
